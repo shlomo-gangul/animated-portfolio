e8d60f38d5dace121a6ca3346345d8be96772a31
shlomo-gangul <shlomo.gangul1@gmail.com>
2025-04-22 09:45:15 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./app.scss";
 import Hero from "./components/hero/Hero";
-import Navbar from "./components/navbar/navbar";
+import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
 import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
